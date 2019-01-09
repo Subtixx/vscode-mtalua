f990ea81170328a126e3ee617b425dddf6bccecb
Dominic Hock <subtixx@gmail.com>
2019-01-09 13:09:30 +0100

    Added search hinting Added verification if resource exists

--- src/features/serverInteractions.ts
+++ src/features/serverInteractions.ts
@@ -7,11 +7,12 @@ import * as fs from 'fs';
 export enum CallType {
     Start,
     Stop,
-    Restart
+    Restart,
+    Search
 }
 
 export function restartResourceSave(doc: vscode.TextDocument) {
-    if(doc.languageId == "mtalua")
+    if (doc.languageId == "mtalua")
         restartResource(doc.uri);
 }
 
@@ -24,11 +25,24 @@ export function startResource(uri: vscode.Uri) {
             prompt: `Type the name of the resource to start, or press ESC to cancel`,
             placeHolder: 'MyAwesomeResource'
         };
+        if (vscode.workspace.getConfiguration("mtalua-http").get("enable_search_hinting", false))
+            options.validateInput = resourcesNameHinting;
         let resName = vscode.window.showInputBox(options);
-        if (resName === undefined)
-            return;
         resName.then((resourceName) => {
-            httpRequest(CallType.Start, resourceName, startResourceCallback);
+            if (resourceName === undefined)
+                return;
+            searchResource(resourceName, (result, response, index, resourceName) => {
+                if (!result) {
+                    vscode.window.showErrorMessage("Resource '" + resourceName + "' not found.");
+                    return;
+                }
+                if (response[1][index] != "loaded") {
+                    vscode.window.showErrorMessage("Resource '" + resourceName + "' already running.");
+                    return;
+                }
+
+                httpRequest(CallType.Start, '["^R^' + resourceName + '"]', startResourceCallback);
+            });
         });
         return;
     }
@@ -37,7 +51,18 @@ export function startResource(uri: vscode.Uri) {
     let folderPath = path.dirname(uri.fsPath + "\\fakePath");
     let resourceName = folderPath.substr(folderPath.lastIndexOf("\\") + 1, folderPath.length - folderPath.lastIndexOf("\\"));
 
-    httpRequest(CallType.Start, resourceName, startResourceCallback);
+    searchResource(resourceName, (result, response, index, resourceName) => {
+        if (!result) {
+            vscode.window.showErrorMessage("Resource '" + resourceName + "' not found.");
+            return;
+        }
+        if (response[1][index] != "loaded") {
+            vscode.window.showErrorMessage("Resource '" + resourceName + "' already running.");
+            return;
+        }
+
+        httpRequest(CallType.Start, '["^R^' + resourceName + '"]', startResourceCallback);
+    });
 }
 
 export function stopResource(uri: vscode.Uri) {
@@ -49,11 +74,24 @@ export function stopResource(uri: vscode.Uri) {
             prompt: `Type the name of the resource to stop, or press ESC to cancel`,
             placeHolder: 'MyAwesomeResource'
         };
+        if (vscode.workspace.getConfiguration("mtalua-http").get("enable_search_hinting", false))
+            options.validateInput = resourcesNameHinting;
         let resName = vscode.window.showInputBox(options);
-        if (resName === undefined)
-            return;
         resName.then((resourceName) => {
-            httpRequest(CallType.Stop, resourceName, stopResourceCallback);
+            if (resourceName === undefined)
+                return;
+            searchResource(resourceName, (result, response, index, resourceName) => {
+                if (!result) {
+                    vscode.window.showErrorMessage("Resource '" + resourceName + "' not found.");
+                    return;
+                }
+                if (response[1][index] != "running") {
+                    vscode.window.showErrorMessage("Resource '" + resourceName + "' not started.");
+                    return;
+                }
+
+                httpRequest(CallType.Stop, '["^R^' + resourceName + '"]', stopResourceCallback);
+            });
         });
         return;
     }
@@ -62,7 +100,18 @@ export function stopResource(uri: vscode.Uri) {
     let folderPath = path.dirname(uri.fsPath + "\\fakePath");
     let resourceName = folderPath.substr(folderPath.lastIndexOf("\\") + 1, folderPath.length - folderPath.lastIndexOf("\\"));
 
-    httpRequest(CallType.Stop, resourceName, stopResourceCallback);
+    searchResource(resourceName, (result, response, index, resourceName) => {
+        if (!result) {
+            vscode.window.showErrorMessage("Resource '" + resourceName + "' not found.");
+            return;
+        }
+        if (response[1][index] != "running") {
+            vscode.window.showErrorMessage("Resource '" + resourceName + "' not started.");
+            return;
+        }
+
+        httpRequest(CallType.Stop, '["^R^' + resourceName + '"]', stopResourceCallback);
+    });
 }
 
 export function restartResource(uri: vscode.Uri) {
@@ -74,27 +123,53 @@ export function restartResource(uri: vscode.Uri) {
             prompt: `Type the name of the resource to restart, or press ESC to cancel`,
             placeHolder: 'MyAwesomeResource'
         };
+        if (vscode.workspace.getConfiguration("mtalua-http").get("enable_search_hinting", false))
+            options.validateInput = resourcesNameHinting;
+
         let resName = vscode.window.showInputBox(options);
-        if (resName === undefined)
-            return;
         resName.then((resourceName) => {
-            httpRequest(CallType.Restart, resourceName, restartResourceCallback);
+            if (resourceName === undefined)
+                return;
+            searchResource(resourceName, (result, response, index, resourceName) => {
+                if (!result) {
+                    vscode.window.showErrorMessage("Resource '" + resourceName + "' not found.");
+                    return;
+                }
+                if (response[1][index] != "running") {
+                    vscode.window.showErrorMessage("Resource '" + resourceName + "' not running.");
+                    return;
+                }
+
+                httpRequest(CallType.Restart, '["^R^' + resourceName + '"]', restartResourceCallback);
+            });
         });
         return;
     }
 
     // fakePath because fsPath contains a path without ending \, adding just \ will not work for some odd reason.
     let folderPath = "";
-    if(fs.statSync(uri.fsPath).isDirectory())
+    if (fs.statSync(uri.fsPath).isDirectory())
         folderPath = path.dirname(uri.fsPath + "\\fakePath");
     else
         folderPath = path.dirname(uri.fsPath);
     let resourceName = folderPath.substr(folderPath.lastIndexOf("\\") + 1, folderPath.length - folderPath.lastIndexOf("\\"));
 
-    httpRequest(CallType.Restart, resourceName, restartResourceCallback);
+    searchResource(resourceName, (result, response, index, resourceName) => {
+        if (!result) {
+            vscode.window.showErrorMessage("Resource '" + resourceName + "' not found.");
+            return;
+        }
+        if (response[1][index] != "running") {
+            vscode.window.showErrorMessage("Resource '" + resourceName + "' not running.");
+            return;
+        }
+
+        httpRequest(CallType.Restart, '["^R^' + resourceName + '"]', restartResourceCallback);
+    });
 }
 
 type HttpCallback = (response: any, resourceName: string) => void;
+type SearchCallback = (result: boolean, response: any, index: number, resourceName: string) => void;
 
 function startResourceCallback(response: any, resourceName: string) {
     if (isArray(response)) {
@@ -123,7 +198,35 @@ function restartResourceCallback(response: any, resourceName: string) {
     }
 }
 
-function httpRequest(callType: CallType, resourceName: string, callback: HttpCallback) {
+function searchResource(resourceName: string, callback: SearchCallback) {
+    httpRequest(CallType.Search, '["' + resourceName + '", ""]', (response: any, formData: string) => {
+        console.log(response);
+        if (isArray(response) && isArray(response[0]) && response[0].length > 0) {
+            let idx = response[0].indexOf("^R^" + resourceName);
+            callback(true, response, idx, resourceName);
+        } else {
+            callback(false, response, -1, resourceName);
+        }
+    });
+}
+
+function resourcesNameHinting(value: string): string | undefined | null | Thenable<string | undefined | null> {
+    return new Promise(resolve => {
+        httpRequest(CallType.Search, '["' + value + '", ""]', (response: any, formData: string) => {
+            if (isArray(response) && response.length > 0 && response[0].length > 0) {
+                let resName = response[0][0].substr(3, response[0][0].length - 3);
+                if (resName === value)
+                    resolve(null); // Valid
+                else
+                    resolve(resName);
+            }
+            else
+                resolve("Not found");
+        });
+    });
+}
+
+function httpRequest(callType: CallType, formData: string, callback: HttpCallback) {
     let callTypeStr = "";
     switch (callType) {
         case CallType.Start:
@@ -135,19 +238,22 @@ function httpRequest(callType: CallType, resourceName: string, callback: HttpCal
         case CallType.Restart:
             callTypeStr = "restartResource";
             break;
+        case CallType.Search:
+            callTypeStr = "getResourcesSearch";
+            break;
 
         default:
             return;
     }
     let opt: request.CoreOptions = getHttpOptions();
-    opt.form = '["^R^' + resourceName + '"]';
+    opt.form = formData;
     request.post("/resourcemanager/call/" + callTypeStr, opt, (error: any, response: request.Response, body: any) => {
-        if (response.statusCode != 200) {
-            vscode.window.showErrorMessage(body);
+        if (response == null || response.statusCode == null || response.statusCode != 200) {
+            vscode.window.showErrorMessage(body == "" ? "Unknown error" : body);
             return;
         }
         let bodyJson = JSON.parse(body);
-        callback(bodyJson, resourceName);
+        callback(bodyJson, formData);
     });
 }
 
